e2f777b9f10aabcb1783951638e5559e8e2d6361
Tin Le <thanhtin.le.vn@gmail.com>
2021-08-23 15:30:02 -0500

    Fix menu, looping logic and input correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 Tin Thanh Le
-Junior Developer
+Software Developer
 Simple calorie counter program with information provided as reference from an
 activity tracking sheet from prevea.com/leadwell
 Reading .xlxs file from Excel and turning it into .csv for easier
@@ -20,15 +20,19 @@ if __name__ == '__main__':
     df = pd.read_csv('resource/activities.csv')
     pd.set_option("display.max_rows", None, "display.max_columns", None)
 
-    weight_choice = menu.weight_selection()
     action = menu.main_menu()
-    if action == 1:
-        print(df['Activities'])
-        action=menu.main_menu()
-    else:
-        activity_number = int(input("Select activity number from provided list(choose 1 to see) "))
-        info = input("Enter your weight and duration in minutes separated by a space: ").split()
-        query = df[info[0]][activity_number]
-        print(f'You will burn {int(query)*int(info[1])/30} calories while { df["Activities"][activity_number]} for {info[1]} minutes')
+    while action in (1,2):
+        if action == 1:
+            print(df['Activities'])
+            action = menu.main_menu()
+        else:
+            activity_number = int(input("Select activity number from provided list(enter '100' to see)"))
+            while activity_number == 100:
+                print(df['Activities'])
+                activity_number = int(input("Select activity number from provided list(enter '100' to see)"))
+            info = input("Enter your weight and duration in minutes separated by a space: ").split()
+            query = df[info[0]][activity_number]
+            print(f'You will burn {int(query)*int(info[1])/30} calories while { df["Activities"][activity_number]} for {info[1]} minutes')
+            action = menu.main_menu()
 
 
--- menu.py
+++ menu.py
@@ -1,10 +1,6 @@
-def weight_selection():
-    return input("Enter 1 or 2 for weight unit:\n"
-                 "1. Pounds (lbs)\n"
-                 "2. Kilograms (kgs)")
-
-
 def main_menu():
     return int(input("1. List activities\n"
                      "2. Choose activity, "
-                     "enter weight and duration of selected activity"))
+                     "enter weight and duration of selected activity\n"
+                     "Enter any other number to finish the program"
+                     ))
